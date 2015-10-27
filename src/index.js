ca1004a850f12438114b71f0d125b2c35948348d
Greg Bergé <neoziro@gmail.com>
2015-10-27 18:26:24 +0100

    Update 2015-10-27T17:26:22.886Z

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Formsy from 'formsy-react';
-import Input from './Input';
-import Select from './Select';
-import Textarea from './Textarea';
-
-export const Form = Formsy.Form;
-export {Input, Select, Textarea};
