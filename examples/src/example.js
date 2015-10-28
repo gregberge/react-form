6026e35a8a6a1f0d1ff138f02e04caf11a3ef925
Greg Bergé <neoziro@gmail.com>
2015-10-28 14:02:26 +0100

    Improve example

--- examples/src/example.js
+++ examples/src/example.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom';
 import Alert from 'react-bootstrap/lib/Alert';
 import Button from 'react-bootstrap/lib/Button';
 import Col from 'react-bootstrap/lib/Col';
-import {Form, Input, Select, Textarea} from '../../src';
+import {addValidationRule, Form, Input, Select, Textarea} from '../../src';
 
 const specialities = {
   doctor: 'Doctor',
   dentist: 'Dentist'
 };
+
+addValidationRule('sameAs', (values, value, field) => value === values[field]);
 
 const Example = React.createClass({
   displayName: 'Example',
@@ -23,7 +25,7 @@ const Example = React.createClass({
   render() {
     return (
       <div>
-        <Form onValidSubmit={this.onValidSubmit} className="form-horizontal">
+        <Form ref="form" onValidSubmit={this.onValidSubmit} className="form-horizontal">
           <fieldset>
             <legend>Basic form with labels</legend>
             {this.renderSuccessMessage()}
@@ -32,6 +34,21 @@ const Example = React.createClass({
             <Input name="zipcode" label="Zipcode" placeholder="Ex: 91200" maxLength={6} validations="isInt" required/>
             <Select name="speciality" label="Speciality" placeholder="Choose a speciality" options={specialities} required/>
             <Textarea name="comment" label="Comment" placeholder="Any comment?" rows={5} required/>
+            <div className="form-group">
+              <Col smOffset={3} sm={9}>
+                <Button bsStyle="primary" type="submit">Submit</Button>
+                <Button bsStyle="default" type="reset">Reset</Button>
+                <Button bsStyle="default" type="button" onClick={this.reset}>Programmatic reset</Button>
+              </Col>
+            </div>
+          </fieldset>
+        </Form>
+
+        <Form className="form-horizontal">
+          <fieldset>
+            <legend>Custom validation</legend>
+            <Input type="password" name="password" label="Password" required/>
+            <Input type="password" name="confirmPassword" label="Confirm password" validations="sameAs:password" required/>
             <div className="form-group">
               <Col smOffset={3} sm={9}>
                 <Button bsStyle="primary" type="submit">Submit</Button>
@@ -58,6 +75,10 @@ const Example = React.createClass({
 
   onValidSubmit() {
     this.setState({success: true});
+  },
+
+  reset() {
+    ReactDOM.findDOMNode(this.refs.form).reset();
   },
 
   /**
